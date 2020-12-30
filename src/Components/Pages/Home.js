3a48d6bbdf4d3ad7f5b858925ac847979d8ee3ed
Femi Akinyemi <santosfemakin@yahoo.com>
2020-12-30 21:38:56 +0100

    Added Experience and Hobby Section

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 import photo1 from "../Images/photo1.jpg";
+import photo2 from "../Images/photo2.jpeg";
+import photo3 from "../Images/photo3.jpeg";
+import photo4 from "../Images/photo4.jpeg";
+import photo5 from "../Images/photo5.jpeg";
+import photo6 from "../Images/photo6.webp";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
@@ -7,7 +12,7 @@ import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
 export default function Home() {
   return (
     <div className="container">
-{/* https://res.cloudinary.com/femakin/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1609350951/photo1_znqchy.jpg */}
+
          {/* Portfolio Header */}
       <div className="portfolio_Header">
         <div className="left_content">
@@ -38,6 +43,144 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+{/* Experience and Skills */}
+
+<div className="experience">
+    <div className="left_experience">
+<div className="exp_top">
+    <h2 className="exp_title">Front end</h2>
+    <figure>
+    <label for="file">Downloading progress:</label>
+<progress id="file" value="32" max="100"> 32% </progress>
+    </figure>
+    <figure>
+    <label for="file">Downloading progress:</label>
+<progress id="file" value="62" max="100"> 32% </progress>
+    </figure>
+    <figure>
+    <label for="file">Downloading progress:</label>
+<progress id="file" value="32" max="100"> 32% </progress>
+    </figure>
+    <figure>
+    <label for="file">Downloading progress:</label>
+<progress id="file" value="32" max="100"> 32% </progress>
+    </figure>
+    <figure>
+    <label for="file">Downloading progress:</label>
+<progress id="file" value="32" max="100"> 32% </progress>
+    </figure>
+</div>
+<div className="exp_bottom">
+    <h2 className="exp_title">Hobbies</h2>
+    <div className="exp_interest">
+<img src={photo2} alt="" className="exp_image"/>
+<h2 className="hobby_interest">Reading</h2>
+<p className="purpose">Try to learn something new every day</p>
+    </div>
+    <div className="exp_interest">
+<img src={photo3} alt="" className="exp_image"/>
+<h2 className="hobby_interest">Reading</h2>
+<p className="purpose">Try to learn something new every day</p>
+    </div>
+    <div className="exp_interest">
+<img src={photo4} alt="" className="exp_image"/>
+<h2 className="hobby_interest">Reading</h2>
+<p className="purpose">Try to learn something new every day</p>
+    </div>
+</div>
+    </div>
+    <div className="right_experience">
+
+<div className="right_exp_top">
+    <p className="right_title_top">Blog</p>
+    <div className="right_top">
+        <h2 className="title_exp">
+        GraphQL for children
+        </h2>
+        <img src={photo5} alt="jpeg" className="blogimge"/>
+    </div>
+    <p className="descr">In this article I tell a story about GraphQL, 
+    focus on how it helps working process become better, especially for frontend developers</p>
+    <a href="/" className="blog_link">google</a>
+</div>
+
+
+
+<div className="right_exp_bottom">
+    <h2 className="exp_title">Experiences</h2>
+    <div className="experiences">
+        <div>
+        <img src={photo6} alt="logo" className="logo"/>
+        <div className="des">
+            <h2 className="date">May 2019 - now</h2>
+            <p className="exp_role">Frontend developer</p>
+            <h2 className="exp_duty">Design reusable React components as a part of overal frontend development
+             in a multi tenant world wide project, working with charts to show high traffic data to customers.</h2>
+        </div>
+        </div>
+
+
+
+        <div>
+        <img src={photo6} alt="logo" className="logo"/>
+        <div className="des">
+            <h2 className="date">May 2019 - now</h2>
+            <p className="exp_role">Frontend developer</p>
+            <h2 className="exp_duty">Design reusable React components as a part of overal frontend development
+             in a multi tenant world wide project, working with charts to show high traffic data to customers.</h2>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
